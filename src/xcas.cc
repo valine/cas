c82c85f84ddf6d5bdc275878a2f421bbe0ed60e9
Lukas Valine <lukas@valine.io>
2021-01-13 22:10:55 -0600

    Intial commit

---
+++ src/xcas.cc
@@ -0,0 +1,13 @@
+#include "giacPCH.h"
+#include "config.h"
+#ifdef HAVE_LIBFLTK
+#include "hist.cxx"
+#else
+#include <stdio.h>
+using namespace std;
+int main(){
+  printf("No GUI support, try icas instead or recompile Giac with\n");
+  printf("./configure --enable-fltk-support\n");
+  printf("make clean ; make\n");
+}
+#endif
